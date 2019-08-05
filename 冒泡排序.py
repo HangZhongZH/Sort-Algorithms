aa6548a6bc58e95033c9d389c6f8ceab09b12316
Hang ZHONG <38694340+HangZhongZH@users.noreply.github.com>
2019-08-05 21:32:58 +0100

    Add files via upload

---
+++ 冒泡排序.py
@@ -0,0 +1,7 @@
+def bubblesort(nums):
+    length = len(nums)
+    for i in range(length - 1):
+        for j in range(length - 1 - i):
+            if nums[j] > nums[j + 1]:
+                nums[j], nums[j + 1] = nums[j + 1], nums[j]
+    return nums
